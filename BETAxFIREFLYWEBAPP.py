c754ff3aec256457cd1f13c2559b5ea37756e195
Cameron <76789715+Cameron-Programer@users.noreply.github.com>
2021-02-04 21:38:15 +0000

    Forgot to name the EGUI boxes
    Updateing the easygui box

--- BETAxFIREFLYWEBAPP.py
+++ BETAxFIREFLYWEBAPP.py
@@ -2,18 +2,20 @@ import requests
 import easygui
 import os
 import sys
-
-Startup = easygui.enterbox("welcome to firefly web app V2.")
+#welcome message 
+Startup = easygui.enterbox("welcome to firefly web app V2.","Firefly Webapp")
 
 if Startup == ("Start"):
     os.startfile(r"C:\Users\Cameron\Documents\Code\Fireflyxwebappxpublic.html")
+   #this stops the program after an if statment there is proably a better way to do this but this is what you deserve
     sys.exit
   
-
+#read the bit in speach marks it will make sense your a big boy you dont need me to spell it out 
 else:
-    easygui.msgbox("When prompted click on 'click here'")
-    easygui.msgbox("Redirecting....")
-    easygui.msgbox("Transfering you now have a good day")
+    easygui.msgbox("When prompted click on 'click here'","Firefly Webapp")
+    easygui.msgbox("Redirecting....","Firefly Webapp")
+    easygui.msgbox("Transfering you now have a good day","Firefly Webapp")
+    #Opens the HTML file 
     os.startfile(r"C:\Users\Cameron\Documents\Code\Fireflyxwebappxpublic.html ")
 
 
